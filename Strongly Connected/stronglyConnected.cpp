6d49e75812a58dd0533a689ce4c5d5c19b8544b5
Atishay25 <atishayjain2552@gmail.com>
2022-10-08 17:47:16 +0530

    Added Strongly Connected Components

---
+++ Strongly Connected/stronglyConnected.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first
+#define S second
+#define pb push_back
+#define pp pair<int,int>
+
+typedef vector<int> vi;
+
+vector <pp> scGraph;        // The SinglyConnectedGraph
+vector <pp> graph[100000];  // The initial Graph given
+bool visited[100000];
+
+
+void dfs(int node = 1){
+    visited[node] = true;
+    for(auto i: graph[node]){
+        if(!visited[i.first]){
+            dfs(i.first);
+        }
+    }
+
+    /**
+     * Edit this normal dfs function to 
+     * check existence of bridge in the graph and its singly connectedness
+     * To construct the scGraph for printing, set a direction to each node
+     * Assigning the direction in which the node was traversed before will work!
+     */
+
+}
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < m; i++){
+        int x, y;
+        cin >> x >> y;
+        graph[x].push_back(y,i);        // An error has been thrown
+        graph[y].push_back(x,i);        // Can you figure it out ? 
+    }
+    dfs();
+    /**
+     * Print the required output after performing modifies dfs traversal
+     * 
+     */
+}
