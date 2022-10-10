aa81d6adf8862ff6d23687203f2d275952efea36
Haibatullah Akhunzada <haibatullah.akhunzada@live.com>
2022-10-10 23:19:49 +0530

    O(n) achieved

--- Symphony/subarray_sum.cpp
+++ Symphony/subarray_sum.cpp
@@ -7,15 +7,20 @@ int max_subarray_sum(int *array, int n) {
     // Go through all possible subarays
     // Maintain a maximum of their sum, return the maximum
     int best_sum = 0;
-    for (int i=0; i<n; i++) {
-        for (int j=i; j<n; j++) {
-            int sum_of_this_subarray = 0;
-            for (int k=i; k<=j; k++) {
-                sum_of_this_subarray += array[k];
-            }
-            best_sum = max(best_sum, sum_of_this_subarray);
-        }
-    }
+	int sum = 0;
+	for (int k = 0; k < n; k++) {
+		sum = max(array[k],sum+array[k]);
+		best_sum = max(best_sum,sum);
+	}
+	// for (int i=0; i<n; i++) {
+	//     for (int j=i; j<n; j++) {
+	//         int sum_of_this_subarray = 0;
+	//         for (int k=i; k<=j; k++) {
+	//             sum_of_this_subarray += array[k];
+	//         }
+	//         best_sum = max(best_sum, sum_of_this_subarray);
+	//     }
+	// }
     return best_sum;
 }
 
